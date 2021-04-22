b57f2bf6b38104ed3fc023114c63bbae94296b0b
Vasilevs1408 <47079886+Vasilevs1408@users.noreply.github.com>
2021-04-22 16:58:32 +0300

    unit 16.7.1

---
+++ cats.py
@@ -0,0 +1,10 @@
+from cat import Cat
+
+cat1 = Cat(name="Baron", gender="male", age=2)
+
+print("cat1.name=", cat1.name)
+print("cat1.getName", cat1.getName())
+print("cat1.gender=", cat1.gender)
+print("cat1.getGender=", cat1.getGender())
+print("cat1.age=", cat1.age)
+print("cat1.getAge=", cat1.getAge())
